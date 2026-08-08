ddc1c72ff429303875dcaf50f153b2a0e9214d06
amoghkrrish <amoghkrrish1067@gmail.com>
2026-08-08 23:41:40 +0530

    add terraform configs for day5

---
+++ day5-terraform/main.tf
@@ -0,0 +1,36 @@
+terraform{
+  required_providers {
+    kubernetes = {
+      sources = "hashicorp/kubernetes"
+      version = "-> 2.0"
+    }
+  }
+}
+
+variable "k8s_token" {
+  description = "kubernetes service account token"
+  type        = string
+  sensitive   = true
+}
+
+provider "kubernetes" {
+  host                   = "https://minikube:8443"
+  token                  = var.k8s_token
+  insecure               = true
+}
+
+resource "kubernetes_namespace" "demo" {
+  metadata {
+    name = "terraform-demo"
+  }
+}
+
+resource "kubernetes_config_map" "demo_config" {
+  metadata {
+    name       = "terraform-demo-config"
+    namespace  = kubernetes_namespace.demo.metadata[0].name
+  }
+  data = {
+    "message" = "Hello from Terraform"
+  }
+}
